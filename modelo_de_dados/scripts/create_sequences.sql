0b75a4a5675cd4d082da5368a51339fe5b863269
Lucas Gomide de Sousa <RM71058@fiap.com.br>
2016-08-23 19:43:08 -0300

    Deleting sequence for tables to log

--- modelo_de_dados/scripts/create_sequences.sql
+++ modelo_de_dados/scripts/create_sequences.sql
@@ -7,20 +7,6 @@ CREATE SEQUENCE seq_post
  NOCACHE
  NOCYCLE;
 /
-drop sequence seq_post_log;
-CREATE SEQUENCE seq_post_log
- START WITH     1
- INCREMENT BY   1
- NOCACHE
- NOCYCLE;
-/
-drop sequence seq_comment_log;
-CREATE SEQUENCE seq_comment_log;
- START WITH     1
- INCREMENT BY   1
- NOCACHE
- NOCYCLE;
-/
 drop sequence seq_comment;
 CREATE SEQUENCE seq_comment;
  START WITH     1

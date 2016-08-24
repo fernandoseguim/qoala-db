5616e062f4fb5018acd2b318f4082b6cca1333b1
Lucas Gomide de Sousa <RM71058@fiap.com.br>
2016-08-23 22:17:59 -0300

    Ordering scripts to execute

---
+++ modelo_de_dados/scripts/3_create_sequences.sql
@@ -0,0 +1,23 @@
+alter session set current_schema = qoala;
+
+drop sequence seq_posts;
+CREATE SEQUENCE seq_posts
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+/
+drop sequence seq_comments;
+CREATE SEQUENCE seq_comments
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+/
+drop sequence seq_users;
+CREATE SEQUENCE seq_users
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+/
